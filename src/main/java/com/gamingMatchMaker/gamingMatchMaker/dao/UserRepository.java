d298ad8bdc09ff7889c64581fefab1612633599d
emadvaid <36285415+emadvaid@users.noreply.github.com>
2019-04-09 22:43:02 -0400

    merged with master

--- src/main/java/com/gamingMatchMaker/gamingMatchMaker/dao/UserRepository.java
+++ src/main/java/com/gamingMatchMaker/gamingMatchMaker/dao/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserRec, UUID> {
-    Optional<List<UserRec>> findByEmail(String email);
+    Optional<UserRec> findByEmail(String email);
 }
